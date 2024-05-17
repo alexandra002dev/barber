4d4ad2fa1b1a4466c0251050ed7e0f4f4bff1bb7
alexandra002dev <alexandramarkess@gmail.com>
2024-05-17 15:52:28 -0300

    feat: add initial search component

---
+++ app/_components/search.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
+
+const SearchInput = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const handleSearchSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!search) return;
+  };
+
+  return (
+    <form className="flex gap-2 " onSubmit={handleSearchSubmit}>
+      <Input
+        placeholder="Buscar um serviço"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Button size="icon" type="submit">
+        <SearchIcon size={20} />
+      </Button>
+    </form>
+  );
+};
+
+export default SearchInput;
--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,22 @@ import Image from "next/image";
 import Header from "./_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import SearchInput from "./_components/search";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="p-5">
-        <h1>Olá, Alexandra</h1>
+      <div className="p-5 ">
+        <h1 className="text-lg">Olá, Alexandra</h1>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
           })}
         </p>
+      </div>
+      <div className="p-5">
+        <SearchInput />
       </div>
     </>
   );
