4993d0a966c1ee95766f81980c3e5b5a9d6de31f
alexandra002dev <alexandramarkess@gmail.com>
2024-05-17 15:38:58 -0300

    feat: add initial header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className="flex justify-between px-5 pt-6">
+      <Link href={"/"}>
+        <Image src="/Logo.png" alt="FSW Foods" height={30} width={150} />
+      </Link>
+
+      <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+        <SheetTrigger onClick={() => setIsMenuOpen(true)}>
+          <MenuIcon />
+        </SheetTrigger>
+        <SheetContent className="w-[90vw] space-y-3">
+          <SheetHeader>
+            <SheetTitle className="text-left">Menu</SheetTitle>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+export default Header;
