4e35a5b7ffa67a6dae2508053b5e8ce5d5273732
alexandra002dev <alexandramarkess@gmail.com>
2024-05-18 20:17:44 -0300

    fix: correção do use client

--- app/_provider/next-auth.tsx
+++ app/_provider/next-auth.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 "use client";
-=======
-"use  client";
->>>>>>> 92cd3db57e92c1e16b0ba05658e9e35bcdbb63da
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
